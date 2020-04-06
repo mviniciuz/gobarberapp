d91dda22f05a0863591f6e3f6f1ebc3e9c222598
Marcus Vinicius <m.viniciuz@hotmail.com>
2020-04-06 18:25:13 -0300

    begin app

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
+
+import Input from '~/components/Input';
+import Button from '~/components/Button';
+
+import Background from '~/components/Background';
+
+import { Container, Form, FormInput, SignLink, SignLinkText, SubmitButton } from './styles';
+
+export default function SignUp({ navigation }) {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+          <FormInput
+            icon="lock-outline"
+            sercureTextEntry
+            placeholder="Sua senha secreta"
+          />
+          <SubmitButton onPress={() => { }}> Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background >
+  );
+}
