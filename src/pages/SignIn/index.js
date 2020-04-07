98c6e845b28856d2b189d017aa95100b40bbf3e0
Marcus Vinicius <m.viniciuz@hotmail.com>
2020-04-06 21:05:25 -0300

    fix: screen signIn signUp - comunication redux

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.png';
 
@@ -8,9 +9,22 @@ import Button from '~/components/Button';
 
 import Background from '~/components/Background';
 
+import { signInRequest } from '~/store/modules/auth/actions';
+
 import { Container, Form, FormInput, SignLink, SignLinkText, SubmitButton } from './styles';
 
 export default function SignIn({ navigation }) {
+  const dispatch = useDispatch();
+  const passwordRef = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit() {
+    dispatch(signInRequest(email, password));
+  }
   return (
     <Background>
       <Container>
@@ -22,13 +36,22 @@ export default function SignIn({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu email"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={t => setEmail(t)}
           />
           <FormInput
             icon="lock-outline"
             sercureTextEntry
             placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubimitEditing={handleSubmit}
+            value={password}
+            onChangeText={t => setPassword(t)}
           />
-          <SubmitButton onPress={() => { }}> Acessar</SubmitButton>
+          <SubmitButton loading={loading} onPress={() => handleSubmit()}> Acessar</SubmitButton>
         </Form>
         <SignLink onPress={() => navigation.navigate('SignUp')}>
           <SignLinkText>Criar conta gratuita</SignLinkText>
