b85cab8e1fd2f0e8849c2b18efae8973cde0e166
Marcus Vinicius <m.viniciuz@hotmail.com>
2020-04-14 20:43:20 -0300

    fix: routes and agendamentos

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,15 +1,120 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+import { signOut } from '~/store/modules/auth/actions/';
+
+import { Container, Title, Separator, Form, FormInput, SubmitButton, LogoutButton } from './styles';
+
 
 export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  const [name, setName] = useState(profile.name);
+  const [email, setEmail] = useState(profile.email);
+  const [oldPassword, setOldPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const emailRef = useRef();
+  const oldPasswordRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  useEffect(() => {
+    setOldPassword('');
+    setPassword('');
+    setConfirmPassword('');
+  }, [profile])
+
+  function handleSubmit() {
+    dispatch(updateProfileRequest({
+      name,
+      email,
+      oldPassword,
+      password,
+      confirmPassword,
+    })
+    );
+  }
+
+  function handleLogout() {
+    dispatch(signOut());
+  }
+
   return (
     <Background>
-      <Text>tela de perfil</Text >
+      <Container>
+        <Title>Meu Perfil</Title>
+        <Form>
+          <FormInput
+            icon="person-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => OldPasswordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <Separator />
+
+          <FormInput
+            icon="lock-outline"
+            sercureTextEntry
+            placeholder="oldPassword"
+            ref={oldPasswordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={oldPassword}
+            onChangeText={setOldPassword}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            sercureTextEntry
+            placeholder="Sua senha atual"
+            ref={passwordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => confirmPasswordRef.current.focus()}
+            value={password}
+            onChangeText={setPassword}
+          />
+
+          <FormInput
+            icon="lock-outline"
+            sercureTextEntry
+            placeholder="Confirmação de senha"
+            ref={confirmPasswordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+          <SubmitButton onPress={handleSubmit}>Atualizar perfil</SubmitButton>
+          <LogoutButton onPress={handleLogout}>Sair do Gobaber</LogoutButton>
+
+        </Form>
+      </Container>
     </Background>
   );
 }
